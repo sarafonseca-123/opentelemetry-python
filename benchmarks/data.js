09e67611524a4ac8ca9c9f9df8c0a6ae2c1474ce
github-action-benchmark <github@users.noreply.github.com>
2021-01-04 18:08:51 +0000

    add OpenTelemetry Python Benchmarks - Python pypy3 - core (pytest) benchmark result for 336af87a095deb46a0de8a9ef39b7f60c87e422c

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609783675712,
+  "lastUpdate": 1609783730992,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -3354,6 +3354,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000009462084171083779",
             "extra": "mean: 5.577776554897531 usec\nrounds: 128206"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anton@ryzhov.me",
+            "name": "Anton Ryzhov",
+            "username": "anton-ryzhov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "336af87a095deb46a0de8a9ef39b7f60c87e422c",
+          "message": "Recreate span on every run of a decorated function (#1451)",
+          "timestamp": "2021-01-04T10:05:15-08:00",
+          "tree_id": "9ff9e572cd69a4b7d44eda02d5b94817e12d3646",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/336af87a095deb46a0de8a9ef39b7f60c87e422c"
+        },
+        "date": 1609783729941,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 173766.54307008933,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000012726353465805754",
+            "extra": "mean: 5.754847753382805 usec\nrounds: 135136"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 96403.2905876247,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000015285922460892652",
+            "extra": "mean: 10.373089900816831 usec\nrounds: 156250"
           }
         ]
       }
