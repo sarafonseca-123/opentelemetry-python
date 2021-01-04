622483c865ec1b794efab6f1a7c4570f67b137a5
github-action-benchmark <github@users.noreply.github.com>
2021-01-04 17:16:26 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - core (pytest) benchmark result for bd8db6e2a461d896c1c0556efd0f4b569106fa96

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609780581868,
+  "lastUpdate": 1609780586613,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -1524,6 +1524,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000014852952898218747",
             "extra": "mean: 47.731354382008185 usec\nrounds: 8124"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bd8db6e2a461d896c1c0556efd0f4b569106fa96",
+          "message": "Add protobuf via gRPC exporting support for Jaeger (#1471)",
+          "timestamp": "2021-01-04T09:13:16-08:00",
+          "tree_id": "ab66aeae2640c477e711409223dda795b083ee77",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/bd8db6e2a461d896c1c0556efd0f4b569106fa96"
+        },
+        "date": 1609780582113,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 25676.020592607914,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000007896025127308266",
+            "extra": "mean: 38.94684522444644 usec\nrounds: 4471"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 19074.23436459067,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001387034890455128",
+            "extra": "mean: 52.426743893657715 usec\nrounds: 5650"
           }
         ]
       }
