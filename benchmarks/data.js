d8d80fdf8ec4c72e2605e469b5eeada3c1a23f77
github-action-benchmark <github@users.noreply.github.com>
2021-01-12 20:18:21 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - core (pytest) benchmark result for db74594d93b7e42d26ed2061b7672d1ad823f30c

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610482695621,
+  "lastUpdate": 1610482700936,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -1712,6 +1712,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000001295801653741069",
             "extra": "mean: 46.93099911026489 usec\nrounds: 6743"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "db74594d93b7e42d26ed2061b7672d1ad823f30c",
+          "message": "Update default port for OTLP exporter to 4317 (#1516)",
+          "timestamp": "2021-01-12T12:14:49-08:00",
+          "tree_id": "319abf2aeb7c5ad6e78c5bf358e34232b37ca479",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/db74594d93b7e42d26ed2061b7672d1ad823f30c"
+        },
+        "date": 1610482699944,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 26672.804222388895,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000011220874197085041",
+            "extra": "mean: 37.491371048290816 usec\nrounds: 3005"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 19253.36877680092,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000013365503372693073",
+            "extra": "mean: 51.93896255729212 usec\nrounds: 6757"
           }
         ]
       }
