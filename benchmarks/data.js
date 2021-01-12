15d73c492d821a3272d2504d6094cc41e36eda99
github-action-benchmark <github@users.noreply.github.com>
2021-01-12 20:18:15 +0000

    add OpenTelemetry Python Benchmarks - Python 3.5 - core (pytest) benchmark result for db74594d93b7e42d26ed2061b7672d1ad823f30c

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610482690875,
+  "lastUpdate": 1610482695621,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -4302,6 +4302,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000016897556795181868",
             "extra": "mean: 66.2923680425932 usec\nrounds: 5426"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "db74594d93b7e42d26ed2061b7672d1ad823f30c",
+          "message": "Update default port for OTLP exporter to 4317 (#1516)",
+          "timestamp": "2021-01-12T12:14:49-08:00",
+          "tree_id": "319abf2aeb7c5ad6e78c5bf358e34232b37ca479",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/db74594d93b7e42d26ed2061b7672d1ad823f30c"
+        },
+        "date": 1610482693691,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 26198.59360524033,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000058755995452142016",
+            "extra": "mean: 38.16998786530192 usec\nrounds: 1813"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 17453.986214186694,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000008689481450637637",
+            "extra": "mean: 57.2935023397231 usec\nrounds: 4915"
           }
         ]
       }
