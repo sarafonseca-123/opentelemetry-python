e232876088d4f14c4e08ea88da3f471b364d8202
github-action-benchmark <github@users.noreply.github.com>
2021-01-18 22:36:55 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - core (pytest) benchmark result for 89fb4e58d0c9d4b3192c91c3371b57007b2d2233

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611009411005,
+  "lastUpdate": 1611009415626,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -2054,6 +2054,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000005576833391733044",
             "extra": "mean: 56.62488874771225 usec\nrounds: 6301"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "89fb4e58d0c9d4b3192c91c3371b57007b2d2233",
+          "message": "Move jaeger propagator out of SDK (#1525)",
+          "timestamp": "2021-01-18T14:34:10-08:00",
+          "tree_id": "dbd3ed5648bccf69082094bcbff56adefd756ada",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/89fb4e58d0c9d4b3192c91c3371b57007b2d2233"
+        },
+        "date": 1611009411122,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 22374.912718893538,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000025515827075955604",
+            "extra": "mean: 44.69291176968895 usec\nrounds: 4239"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 15780.29212218426,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000031351060719186154",
+            "extra": "mean: 63.37018302685154 usec\nrounds: 8873"
           }
         ]
       }
