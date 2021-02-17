b0ce48e29687d3fe5497d9d71995ed82d607659d
github-action-benchmark <github@users.noreply.github.com>
2021-02-17 20:05:31 +0000

    add OpenTelemetry Python Benchmarks - Python 3.5 - exporter (pytest) benchmark result for 03c6b7391d16236ad5d4e48b0cb0bff786c2e583

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1613592326641,
+  "lastUpdate": 1613592330954,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -25478,6 +25478,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0009171060286977986",
             "extra": "mean: 355.7545610765953 usec\nrounds: 3864"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rob.knox.dev@gmail.com",
+            "name": "Rob Knox",
+            "username": "robwknox"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03c6b7391d16236ad5d4e48b0cb0bff786c2e583",
+          "message": "Zipkin exporter add support for v1 api json format (#1411)",
+          "timestamp": "2021-02-17T12:03:29-08:00",
+          "tree_id": "d7a5fe453f77df1d3e7d772741c3e902040de07a",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/03c6b7391d16236ad5d4e48b0cb0bff786c2e583"
+        },
+        "date": 1613592324282,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_simple_span_processor",
+            "value": 2222.520509468363,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000011337611262900844",
+            "extra": "mean: 449.9396049394408 usec\nrounds: 81"
+          },
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_batch_span_processor",
+            "value": 2702.1376721261736,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00102809059844446",
+            "extra": "mean: 370.0773688607625 usec\nrounds: 4072"
           }
         ]
       }
