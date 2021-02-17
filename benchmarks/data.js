67d4433951b76472342313ad9d00b6bd11fb7c5c
github-action-benchmark <github@users.noreply.github.com>
2021-02-17 20:07:56 +0000

    add OpenTelemetry Python Benchmarks - Python pypy3 - core (pytest) benchmark result for 03c6b7391d16236ad5d4e48b0cb0bff786c2e583

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1613592357739,
+  "lastUpdate": 1613592476347,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -16502,6 +16502,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000005687863916046933",
             "extra": "mean: 5.175612722346831 usec\nrounds: 140846"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rob.knox.dev@gmail.com",
+            "name": "Rob Knox",
+            "username": "robwknox"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "03c6b7391d16236ad5d4e48b0cb0bff786c2e583",
+          "message": "Zipkin exporter add support for v1 api json format (#1411)",
+          "timestamp": "2021-02-17T12:03:29-08:00",
+          "tree_id": "d7a5fe453f77df1d3e7d772741c3e902040de07a",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/03c6b7391d16236ad5d4e48b0cb0bff786c2e583"
+        },
+        "date": 1613592473526,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 226056.96746095494,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004662923710106201",
+            "extra": "mean: 4.423663695182154 usec\nrounds: 158731"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 130038.18549410367,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00006481353459934139",
+            "extra": "mean: 7.690048859112564 usec\nrounds: 192308"
           }
         ]
       }
