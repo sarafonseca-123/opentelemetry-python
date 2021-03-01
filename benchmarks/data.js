77859d1fc35f920de720d78bd202df1ab1ecdf8b
github-action-benchmark <github@users.noreply.github.com>
2021-03-01 18:01:40 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - core (pytest) benchmark result for 326b7b0d0e9bc1cfc21d70d3d09e58b38c5cc8a3

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614621696409,
+  "lastUpdate": 1614621700747,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -8934,6 +8934,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00004582317434637474",
             "extra": "mean: 66.8866043872524 usec\nrounds: 6610"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "owais@users.noreply.github.com",
+            "name": "Owais Lone",
+            "username": "owais"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "326b7b0d0e9bc1cfc21d70d3d09e58b38c5cc8a3",
+          "message": "Add `max_tag_value_length` support to Jaeger exporter (#1633)\n\nThis commit adds ability to specifiy maximum length for string\r\nattributes to the Jaeger exporter. This is similar to how Zipkin\r\nexporter supports `max_tag_value_length`.",
+          "timestamp": "2021-03-01T09:56:10-08:00",
+          "tree_id": "5ded5223f11a99a03cd05007da1ffe872d1fb4c8",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/326b7b0d0e9bc1cfc21d70d3d09e58b38c5cc8a3"
+        },
+        "date": 1614621690815,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 22586.59142199017,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002075987489554824",
+            "extra": "mean: 44.274055403791735 usec\nrounds: 4025"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 15527.637714966875,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003246313441328289",
+            "extra": "mean: 64.40129647255446 usec\nrounds: 9242"
           }
         ]
       }
