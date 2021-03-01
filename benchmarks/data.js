ead1c014d5cc4153c5f8572ad9c67d08888b70e4
github-action-benchmark <github@users.noreply.github.com>
2021-03-01 22:27:26 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - core (pytest) benchmark result for dc59139a3ac82bc6a8d0c738abf0e36676806b5a

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1614637641333,
+  "lastUpdate": 1614637646226,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -6082,6 +6082,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000016697292401024654",
             "extra": "mean: 58.45718245862488 usec\nrounds: 6955"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ocelotl@users.noreply.github.com",
+            "name": "Diego Hurtado",
+            "username": "ocelotl"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dc59139a3ac82bc6a8d0c738abf0e36676806b5a",
+          "message": "Refactor utils (#1599)",
+          "timestamp": "2021-03-01T14:24:56-08:00",
+          "tree_id": "00f82a2e40fdd44f94c5258dfefcf665c282c369",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/dc59139a3ac82bc6a8d0c738abf0e36676806b5a"
+        },
+        "date": 1614637639718,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 18197.54352869615,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003054457461390936",
+            "extra": "mean: 54.95247193243832 usec\nrounds: 4026"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 12927.196558853866,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003722518469573725",
+            "extra": "mean: 77.3562926383368 usec\nrounds: 7118"
           }
         ]
       }
