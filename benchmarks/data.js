e4b5ccd5a64c8fc2a3a16c189e67dfdbe8ad9e56
github-action-benchmark <github@users.noreply.github.com>
2021-03-08 17:38:23 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - exporter (pytest) benchmark result for 9254c170d644c853f7824e27e733e0930b4ee8f2

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615225100241,
+  "lastUpdate": 1615225103123,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -22356,6 +22356,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0009013376105588526",
             "extra": "mean: 273.18913521695447 usec\nrounds: 5946"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "owais@users.noreply.github.com",
+            "name": "Owais Lone",
+            "username": "owais"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9254c170d644c853f7824e27e733e0930b4ee8f2",
+          "message": "Move `use_span()` method from Tracer to `opentelemetry.trace.use_span()` (#1668)\n\nFixes: #1630",
+          "timestamp": "2021-03-08T09:34:01-08:00",
+          "tree_id": "9ecc0b7ca049daaea4d01470ba39b169ea6ab001",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/9254c170d644c853f7824e27e733e0930b4ee8f2"
+        },
+        "date": 1615225097794,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_simple_span_processor",
+            "value": 2861.2021020870197,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000019309435934317816",
+            "extra": "mean: 349.5034479635603 usec\nrounds: 221"
+          },
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_batch_span_processor",
+            "value": 4014.7973827890123,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008504666774100314",
+            "extra": "mean: 249.07857225544888 usec\nrounds: 5356"
           }
         ]
       }
