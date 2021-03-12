736dbe514dc5fbaae4dcc4d4e921ffcee9474468
github-action-benchmark <github@users.noreply.github.com>
2021-03-12 02:57:09 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - core (pytest) benchmark result for a46b4c2039275b4c480b3ef3a3ca530fd443c0a2

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1615517822747,
+  "lastUpdate": 1615517829550,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -3686,6 +3686,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000004423311465834919",
             "extra": "mean: 49.61323145146656 usec\nrounds: 6200"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lechen@microsoft.com",
+            "name": "Leighton Chen",
+            "username": "lzchen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a46b4c2039275b4c480b3ef3a3ca530fd443c0a2",
+          "message": "Update docs to specify to use Resource APIs to work with Resource (#1686)",
+          "timestamp": "2021-03-11T18:55:08-08:00",
+          "tree_id": "7ae56ab818db51d52dbcaf0b79c773239f4b8595",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/a46b4c2039275b4c480b3ef3a3ca530fd443c0a2"
+        },
+        "date": 1615517824797,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 26080.03635141152,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001986460421795423",
+            "extra": "mean: 38.34350483740324 usec\nrounds: 5168"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 18551.35100303785,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003511979190576082",
+            "extra": "mean: 53.9044299165191 usec\nrounds: 7541"
           }
         ]
       }
