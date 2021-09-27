8e8384dfaed972948cbe2fbf9a709576a0452f90
github-action-benchmark <github@users.noreply.github.com>
2021-09-27 16:11:01 +0000

    add OpenTelemetry Python Benchmarks - Python 3.6 - propagator (pytest) benchmark result for a462f554bb7bc2d20a3780ce663f2c4904480a36

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1632759058998,
+  "lastUpdate": 1632759060975,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -59584,44 +59584,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "lechen@microsoft.com",
-            "name": "Leighton Chen",
-            "username": "lzchen"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "0fb4fd1d7c63d72c9be56d884f428938b522232f",
-          "message": "[chore] Update main after 1.1.0 release (#1786)",
-          "timestamp": "2021-04-21T07:52:32-07:00",
-          "tree_id": "fb4bccc16291509b91a2374aef84e19efa9e9e48",
-          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/0fb4fd1d7c63d72c9be56d884f428938b522232f"
-        },
-        "date": 1619016832752,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_extract_single_header",
-            "value": 47405.65064724919,
-            "unit": "iter/sec",
-            "range": "stddev: 0.000009850813611451466",
-            "extra": "mean: 21.0945316928801 usec\nrounds: 8819"
-          },
-          {
-            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_inject_empty_context",
-            "value": 97595.63614444998,
-            "unit": "iter/sec",
-            "range": "stddev: 0.000007007196545884456",
-            "extra": "mean: 10.246359770839687 usec\nrounds: 6632"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "ocelotl@users.noreply.github.com",
             "name": "Diego Hurtado",
             "username": "ocelotl"
@@ -63378,6 +63340,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000019531892967763124",
             "extra": "mean: 8.75398996953881 usec\nrounds: 45661"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a462f554bb7bc2d20a3780ce663f2c4904480a36",
+          "message": "update to pylint 2.11.0 (#2119)\n\n* test pylint 2.11.0\r\n\r\n* address last few pylint issues\r\n\r\n* fix mypy\r\n\r\nCo-authored-by: Srikanth Chekuri <srikanth.chekuri92@gmail.com>",
+          "timestamp": "2021-09-27T16:09:22Z",
+          "tree_id": "9eceed3e47a07e6e5116855acec3b3efba2f8eb5",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/a462f554bb7bc2d20a3780ce663f2c4904480a36"
+        },
+        "date": 1632759041909,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_extract_single_header",
+            "value": 63920.95977597394,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000007547069826358976",
+            "extra": "mean: 15.644320790938302 usec\nrounds: 8850"
+          },
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_inject_empty_context",
+            "value": 103113.74214500221,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000530826516135094",
+            "extra": "mean: 9.69802840240018 usec\nrounds: 35842"
           }
         ]
       }
