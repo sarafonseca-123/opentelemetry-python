800f03c03687cb68c734e9e940fbe1462e1c67f5
github-action-benchmark <github@users.noreply.github.com>
2021-10-16 22:38:49 +0000

    add OpenTelemetry Python Benchmarks - Python pypy-3.7 - sdk (pytest) benchmark result for 1e001868e3eafade5974f0fe6d67986cbf4e2d2a

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634423915764,
+  "lastUpdate": 1634423929554,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -69050,6 +69050,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000007841920746283077",
             "extra": "mean: 4.225803747372079 usec\nrounds: 144928"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "owais@users.noreply.github.com",
+            "name": "Owais Lone",
+            "username": "owais"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1e001868e3eafade5974f0fe6d67986cbf4e2d2a",
+          "message": "Remove left-over bootstrap_gen.py file (#2213)\n\nCo-authored-by: Srikanth Chekuri <srikanth.chekuri92@gmail.com>",
+          "timestamp": "2021-10-17T04:07:20+05:30",
+          "tree_id": "d18722489dc44731b2def980573223f7aeab9717",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/1e001868e3eafade5974f0fe6d67986cbf4e2d2a"
+        },
+        "date": 1634423912210,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 306796.1969134809,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001844799463934674",
+            "extra": "mean: 3.259492816601033 usec\nrounds: 37879"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 203204.64564290125,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000041327202076098814",
+            "extra": "mean: 4.921147333202882 usec\nrounds: 120482"
           }
         ]
       }
