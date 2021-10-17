acdb66e525dcef9901bc0c2de2138c69bacba6fe
github-action-benchmark <github@users.noreply.github.com>
2021-10-17 18:28:51 +0000

    add OpenTelemetry Python Benchmarks - Python 3.8 - sdk (pytest) benchmark result for a1feaf4709a456b108b45b1cb23acfe9f927a445

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1634495329315,
+  "lastUpdate": 1634495331166,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -65700,6 +65700,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000044642314783941294",
             "extra": "mean: 64.64237039141345 usec\nrounds: 7052"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thanos@diacakis.com",
+            "name": "Thanos Diacakis",
+            "username": "thanosd"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a1feaf4709a456b108b45b1cb23acfe9f927a445",
+          "message": "Fixing typo (#2216)",
+          "timestamp": "2021-10-17T18:27:39Z",
+          "tree_id": "05fe779ad808e1cbf78231f395595d6c43cf991f",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/a1feaf4709a456b108b45b1cb23acfe9f927a445"
+        },
+        "date": 1634495309935,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 21905.986741907556,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00008652492530754368",
+            "extra": "mean: 45.64962134697799 usec\nrounds: 4722"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 16354.512019420523,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002058685817455903",
+            "extra": "mean: 61.145205605188835 usec\nrounds: 8599"
           }
         ]
       }
