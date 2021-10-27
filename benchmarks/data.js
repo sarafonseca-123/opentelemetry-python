2a30bdb32dafc200a60aaf453d039f231ad233c8
github-action-benchmark <github@users.noreply.github.com>
2021-10-27 20:28:47 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - propagator (pytest) benchmark result for 05e9fe171fdfd6a4d7d2336f6ac41bab69c8a71f

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635366524148,
+  "lastUpdate": 1635366527462,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -48178,44 +48178,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "ell22@duke.edu",
-            "name": "Eddy Lin",
-            "username": "eddyleelin"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "038bd2499f48671ba293ab16be8b0bb0dc33464d",
-          "message": "Update otel-exporter OTLP headers parsing to match format specs (#1869)",
-          "timestamp": "2021-05-26T09:56:53-07:00",
-          "tree_id": "7ea677de6191898af0fca5683ca714acf10b8b26",
-          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/038bd2499f48671ba293ab16be8b0bb0dc33464d"
-        },
-        "date": 1622048269400,
-        "tool": "pytest",
-        "benches": [
-          {
-            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_extract_single_header",
-            "value": 110650.24687538177,
-            "unit": "iter/sec",
-            "range": "stddev: 0.0000037744766610294403",
-            "extra": "mean: 9.037485484566838 usec\nrounds: 7716"
-          },
-          {
-            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_inject_empty_context",
-            "value": 161691.39819776415,
-            "unit": "iter/sec",
-            "range": "stddev: 0.000009099829853500631",
-            "extra": "mean: 6.184620896016396 usec\nrounds: 58480"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "aboten@lightstep.com",
             "name": "alrex",
             "username": "codeboten"
@@ -51972,6 +51934,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000075914973766320635",
             "extra": "mean: 5.17404486586928 usec\nrounds: 68961"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nathanielruiz98@gmail.com",
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "05e9fe171fdfd6a4d7d2336f6ac41bab69c8a71f",
+          "message": "Remove lingering distro pkg files (#2241)",
+          "timestamp": "2021-10-27T13:27:12-07:00",
+          "tree_id": "ffbb45c360e18c0f163822b86bb86ceb85980cca",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/05e9fe171fdfd6a4d7d2336f6ac41bab69c8a71f"
+        },
+        "date": 1635366499542,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_extract_single_header",
+            "value": 99526.06714457032,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000006399564586166681",
+            "extra": "mean: 10.04761896747525 usec\nrounds: 7729"
+          },
+          {
+            "name": "propagator/opentelemetry-propagator-b3/tests/performance/benchmarks/trace/propagation/test_benchmark_b3_format.py::test_inject_empty_context",
+            "value": 189859.16159138212,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000056171944974763375",
+            "extra": "mean: 5.267062129728645 usec\nrounds: 66667"
           }
         ]
       }
