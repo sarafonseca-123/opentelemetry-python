562a8900f5066c0712949ad6d3613e4f03e99919
github-action-benchmark <github@users.noreply.github.com>
2021-11-04 00:33:30 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - sdk (pytest) benchmark result for 64e884aef5294eec8a2d94d067113e78d04b7395

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1635986004625,
+  "lastUpdate": 1635986009861,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -68552,6 +68552,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00000904101546569472",
             "extra": "mean: 61.711058264473614 usec\nrounds: 4840"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "Alex Boten",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "lechen@microsoft.com",
+            "name": "Leighton Chen",
+            "username": "lzchen"
+          },
+          "distinct": true,
+          "id": "64e884aef5294eec8a2d94d067113e78d04b7395",
+          "message": "move log_exporter to _log_exporter as it's still experimental (#2252)",
+          "timestamp": "2021-11-03T17:32:17-07:00",
+          "tree_id": "893bf2287e5282d97a8efa6f14140fc2338967b2",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/64e884aef5294eec8a2d94d067113e78d04b7395"
+        },
+        "date": 1635985987811,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 18674.32803048654,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000037244858881687015",
+            "extra": "mean: 53.54945025960037 usec\nrounds: 4624"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 13813.215034476723,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003302250226529953",
+            "extra": "mean: 72.39444238753084 usec\nrounds: 6266"
           }
         ]
       }
