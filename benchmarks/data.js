03d1e6998577180169f4c7a86f161e7d43e70eae
github-action-benchmark <github@users.noreply.github.com>
2021-11-22 16:40:18 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - sdk (pytest) benchmark result for 80f5a20ba8f3a71450fe3020fecf362fedb76bff

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637599216072,
+  "lastUpdate": 1637599218018,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -70300,6 +70300,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000002274757948348978",
             "extra": "mean: 72.94459057768974 usec\nrounds: 1783"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80f5a20ba8f3a71450fe3020fecf362fedb76bff",
+          "message": "Improve log message in OTLP exporters (#2285)",
+          "timestamp": "2021-11-22T10:38:54-06:00",
+          "tree_id": "b11c1e7e7e8b4985f6ce0420e6d9b7ba14aac220",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/80f5a20ba8f3a71450fe3020fecf362fedb76bff"
+        },
+        "date": 1637599191734,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 15636.544947964798,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000015856322549446986",
+            "extra": "mean: 63.95274680741776 usec\nrounds: 4072"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 11348.949897223529,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000016861575871864595",
+            "extra": "mean: 88.11387917437592 usec\nrounds: 1647"
           }
         ]
       }
