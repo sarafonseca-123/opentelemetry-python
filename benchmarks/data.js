b806b573d2adecc4e4d467f25c16383609913fc2
github-action-benchmark <github@users.noreply.github.com>
2022-01-18 16:37:42 +0000

    add OpenTelemetry Python Benchmarks - Python 3.10 - sdk (pytest) benchmark result for 06aa56489b646a8b20f05944d1839d1eb7143160

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642523840429,
+  "lastUpdate": 1642523862074,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.8 - core": [
@@ -87828,6 +87828,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000024083673788565906",
             "extra": "mean: 57.02981461377426 usec\nrounds: 7924"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "Alex Boten",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "06aa56489b646a8b20f05944d1839d1eb7143160",
+          "message": "[exporter/otlp] add support for Sum (#2370)\n\n* [exporter/otlp] add support for Sum\r\n\r\nAdding support for exporting Sum datapoints.\r\n\r\n* update based on feedback\r\n\r\n* update changelog\r\n\r\n* clean up imports\r\n\r\n* fix lint\r\n\r\n* fix lint\r\n\r\n* feedback update",
+          "timestamp": "2022-01-18T10:34:02-06:00",
+          "tree_id": "13dee599c7a87e0501802d89694f8d82ad1ad317",
+          "url": "https://github.com/open-telemetry/opentelemetry-python/commit/06aa56489b646a8b20f05944d1839d1eb7143160"
+        },
+        "date": 1642523831957,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 29507.59488914192,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000027569791704961123",
+            "extra": "mean: 33.88958008122769 usec\nrounds: 5663"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 21207.666163059304,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000004980779463056516",
+            "extra": "mean: 47.15276034200574 usec\nrounds: 7252"
           }
         ]
       }
